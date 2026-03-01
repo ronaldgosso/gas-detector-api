0c12d502af00911836169ea6e083ed47643e640e
Ronald Gosso <ronald.gosso@neptuneprolabs.com>
2026-03-01 10:03:39 +0300

    fix: Update CORS origin path for the gas detector API.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // ===== MIDDLEWARE =====
 app.use(cors(
     {
-        origin: 'https://ronaldgosso.github.io/gas-detector-api/',
+        origin: 'https://ronaldgosso.github.io/gas-detector/',
     }
 ));
 app.use(bodyParser.json());
